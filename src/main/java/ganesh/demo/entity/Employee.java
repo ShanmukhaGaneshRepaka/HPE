e60171e582e94a11381c6b9476664cf0536f7fc6
ShanmukhaGaneshRepaka <shanmukhaganeshrepaka@gmail.com>
2026-05-08 17:48:19 +0530

    Implemented create employee api to store small part of data as a xml string followed good structure

--- src/main/java/ganesh/demo/entity/Employee.java
+++ src/main/java/ganesh/demo/entity/Employee.java
@@ -1,17 +1,21 @@
-package ganesh.demo;
+package ganesh.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Employee {
-	Employee(){
-		System.out.println("Employee object created");
-	}
+	
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
+	
+	private String employeeDetails;
+	
 	
 	public int getId() {
 		return id;
@@ -30,6 +34,12 @@ public class Employee {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public String getEmployeeDetails() {
+		return employeeDetails;
+	}
+	public void setEmployeeDetails(String employeeDetails) {
+		this.employeeDetails = employeeDetails;
 	}
 
 }
